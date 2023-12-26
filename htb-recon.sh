938c900a18fa1bc9d31ec578a37b534f71fa0698
dipsylala <github@cus-productions.com>
2023-12-26 23:27:26 +0000

    Better file descriptions

--- htb-recon.sh
+++ htb-recon.sh
@@ -47,17 +47,17 @@ then
   tmux send-keys -t htb_recon:VPN 'sudo openvpn ~/HackingProjects/hackthebox/lab.ovpn'
 
   ## Scan for ports - nmap for quick scan, masscan for deep scan
-  tmux send-keys -t htb_recon:PORTS.0 'nmap -T4 -sC -sV -oN recon/initial_nmap.txt -v -Pn $target' 
-  tmux send-keys -t htb_recon:PORTS.1 'sudo masscan -p1-65535 -e tun0 -oL recon/allports.txt --rate=1000 -vv -Pn $target'
+  tmux send-keys -t htb_recon:PORTS.0 'nmap -T4 -sC -sV -oN recon/ports_initial_nmap.txt -v -Pn $target' 
+  tmux send-keys -t htb_recon:PORTS.1 'sudo masscan -p1-65535 -e tun0 -oL recon/ports_full_masscan.txt --rate=1000 -vv -Pn $target'
 
   ## Scan for directories
-  tmux send-keys -t htb_recon:DIRS.0 'gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt'
-  tmux send-keys -t htb_recon:DIRS.1 'ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/ffuf.txt' 
+  tmux send-keys -t htb_recon:DIRS.0 'gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dir_gobuster.txt'
+  tmux send-keys -t htb_recon:DIRS.1 'ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/dir_ffuf.txt' 
 
   ## DNS Window: Running Commands
   tmux send-keys -t htb_recon:DNS.0 'echo "${target}\t${name}.htb"  | sudo tee -a /etc/hosts' 
-  tmux send-keys -t htb_recon:DNS.1 'gobuster dns -d ${name}.htb -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-20000.txt -o recon/dnsscan.txt'
-  tmux send-keys -t htb_recon:DNS.2 'wfuzz -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-20000.txt -u http://${name}.htb/ -H "Host: FUZZ.${name}.htb" --hc 301,302'
+  tmux send-keys -t htb_recon:DNS.1 'gobuster dns -d ${name}.htb -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-20000.txt -o recon/dns_gobuster.txt'
+  tmux send-keys -t htb_recon:DNS.2 'wfuzz -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-20000.txt -u http://${name}.htb/ -H "Host: FUZZ.${name}.htb" --hc 301,302 -f recon/dns_wfuzz.txt'
 
   ## Create notes in MarkDown, ready for VS Code
   echo "# Info" >> Notes.md
