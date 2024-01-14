8a847efbba8b5cc7b0dda297c213c28ada8f17a0
dipsylala <github@cus-productions.com>
2024-01-14 15:00:02 +0000

    DNS now includes dig

--- htb-recon.sh
+++ htb-recon.sh
@@ -51,23 +51,24 @@ then
   tmux send-keys -t htb_recon:PORTS.1 'sudo masscan -p1-65535 -e tun0 -oL recon/ports_full_masscan.txt --rate=1000 -vv -Pn $target'
 
   ## Scan for directories
-  tmux send-keys -t htb_recon:DIRS.0 'gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dir_gobuster.txt'
-  tmux send-keys -t htb_recon:DIRS.1 'ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/dir_ffuf.txt' 
+  tmux send-keys -t htb_recon:DIRS.0 'gobuster dir -u http://${name}.htb -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dir_gobuster.txt'
+  tmux send-keys -t htb_recon:DIRS.1 'ffuf -w /usr/share/wordlists/dirb/big.txt -u http://${name}.htb/FUZZ | tee recon/dir_ffuf.txt' 
 
   ## DNS Window: Running Commands
   tmux send-keys -t htb_recon:DNS.0 'echo "${target}\t${name}.htb"  | sudo tee -a /etc/hosts' 
-  tmux send-keys -t htb_recon:DNS.1 'gobuster dns -d ${name}.htb -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-20000.txt -o recon/dns_gobuster.txt'
-  tmux send-keys -t htb_recon:DNS.2 'wfuzz -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-20000.txt -u http://${name}.htb/ -H "Host: FUZZ.${name}.htb" --hc 301,302 -f recon/dns_wfuzz.txt'
+  tmux send-keys -t htb_recon:DNS.1 'dig any ${name}.htb @${target}' 
+  tmux send-keys -t htb_recon:DNS.2 'gobuster dns -d ${name}.htb -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-20000.txt -o recon/dns_gobuster.txt'
+  tmux send-keys -t htb_recon:DNS.3 'wfuzz -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-20000.txt -u http://${name}.htb/ -H "Host: FUZZ.${name}.htb" --hc 301,302 -f recon/dns_wfuzz.txt'
 
   ## Create notes in MarkDown, ready for VS Code
   echo "# Info" >> Notes.md
+  echo "* Name: $name" >> Notes.md
   echo "* IP: $target" >> Notes.md
   echo "* Box: $box" >> Notes.md
   echo "* Level: " >> Notes.md
   code  Notes.md
 
-  echo "To connect, use:"
-  echo "tmux a -t htb_recon"
+  echo "'tmux a -t htb_recon' to connect"
 else
   echo "Usage: ./htb-recon.sh <IP> <Name_of_Machine> <OS> "
   echo "Example: ./htb-recon.sh 10.10.10.180 ServMon Windows"
